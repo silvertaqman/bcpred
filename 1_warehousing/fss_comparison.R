8bb7660cf78ad68c05627dfaafe27c6c7f14cbb2
silvertaqman <ajcedeno@espe.edu.ec>
2022-11-06 18:53:12 -0500

    nearly fullfilled dendroplot for comparison at methods of selection

--- 1_warehousing/fss_comparison.R
+++ 1_warehousing/fss_comparison.R
@@ -3,14 +3,12 @@ library(readr)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
-library(eulerr)
+library(grid)
+library(ggpubr)
 library(pilot)
-library(chorddiag)
 library(igraph)
 library(ggraph)
-library(htmlwidgets)
-library(manipulateWidget)
-library(htmltools)
+library(ggdendro)
 
 # TilePlot
 df <- read_csv("./selection.csv.gz") %>%
@@ -31,7 +29,7 @@ b <- df %>%
         count(Coincidence) %>% 
         arrange(desc(n))
 
-df %>%
+tile <- df %>%
 	count(Method, Coincidence) %>%
 	mutate(
 		Method = factor(Method, levels=a$Method),
@@ -40,10 +38,74 @@ df %>%
   geom_tile()+
   theme(
   	axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
-  	axis.text.y=element_text(size=5))
+  	axis.text.y=element_text(size=5),
+  	legend.position="none")
 
 ggsave("tile.pdf", 
+	tile,
 	width=1500,
+	height= 12000, 
+	units="px", 
+	useDingbats=FALSE)
+
+# Plot a dendrogram
+
+scores <- df %>%
+	count(Method, Coincidence) %>%
+	mutate(
+		Method = factor(Method, levels=a$Method),
+		Coincidence = factor(Coincidence, levels=b$Coincidence)) %>%
+		with(table(Coincidence, Method))
+
+d = dist(scores, method = "binary")
+hc = hclust(d, method="ward.D")
+pdf("dendrogram.pdf", width=45, height=10)
+plot(hc, cex=0.5)
+dev.off()
+
+# Merge them requires to change order
+long <- df %>% 
+	count(Method, Coincidence) %>%
+	mutate(
+		Coincidence = factor(
+			Coincidence,
+			levels=partition_leaves(dendro)[[1]]))
+dendro <- as.dendrogram(hc)
+
+longtile <- long %>%
+	ggplot(aes(Method, Coincidence, fill= n)) + 
+  geom_tile()+
+  theme(
+  	axis.text.x = element_text(
+  		angle = 90,
+  		vjust = 0.5,
+  		hjust=1),
+  	axis.text.y = element_blank(),
+  	axis.title.y = element_blank(),
+  	axis.ticks.y = element_blank(),
+  	legend.position="none")
+
+dendroplot <- 
+	ggdendrogram(
+		dendro,
+		rotate=TRUE)+
+	theme(
+		axis.text.y = element_text(
+			size = 5,
+			vjust = 0.5,
+			hjust = 1.0))
+
+dendrotile <- ggarrange(
+	longtile,
+	dendroplot,
+	labels=c("A","B"),
+	ncol=2,
+	nrow=1)
+#	align="hv")
+
+ggsave("dendrotile.pdf",
+	dendrotile,
+	width=2000,
 	height= 12000, 
 	units="px", 
 	useDingbats=FALSE)
@@ -70,6 +132,7 @@ ggplot() +
   theme_pilot() + 
   theme(legend.position="none") +
   coord_equal()
+
 # For coincidences
 packing <- circleProgressiveLayout(b$n, sizetype='area')
 data <- cbind(b, packing)
@@ -88,96 +151,3 @@ ggplot() +
   theme_pilot() + 
   theme(legend.position="none") +
   coord_equal()
-
-# Dendrogram
-scores <- df %>%
-	count(Method, Coincidence) %>%
-	mutate(
-		Method = factor(Method, levels=a$Method),
-		Coincidence = factor(Coincidence, levels=b$Coincidence)) %>%
-		with(table(Coincidence, Method))
-
-d = dist(scores, method = "binary")
-hc = hclust(d, method="ward.D")
-pdf("dendrogram.pdf", width=45, height=10)
-plot(hc, cex=0.5)
-dev.off()
-# Otra opcion
-
-d %>%
-	hclust() %>%
-	as.dendrogram() %>%
-	ggraph("dendogram")+
-		geom_edge_elbow()
-
-# Chord Diagram
-# circlize::chordDiagram(scores, transparency = 0.5) # doesn't work for +360 features , so first 360 features are selected
-
-order <- levels(b$Coincidence)[1:50]
-
-df %>%
-	count(Method, Coincidence) %>%
-	mutate(
-		Method = factor(Method, levels=a$Method),
-		Coincidence = factor(Coincidence, levels=b$Coincidence)) %>%
-		filter(Coincidence %in% order) %>% # %>% barplot(Coincidence)
-		graph_from_data_frame() %>%
-		ggraph(aes(colour=Method),layout="linear", circular=TRUE)+
-			geom_edge_arc(
-				edge_colour="black",
-				edge_alpha=0.3, 
-				edge_width=0.2)+
-			geom_node_point(
-				color="#69b3a2", 
-				size=5) +
-			geom_node_text(
-				aes(label=name),
-				repel = TRUE, 
-				size=8, 
-				color="#69b3a2") +
-		theme_pilot()+
-		theme(
-		  legend.position="none",
-		  plot.margin=unit(rep(2,4), "cm"))
-  
-
-
-# Sankey Diagram
-links <- df %>% 
-	count(Method, Coincidence) %>%
-	mutate(
-		Method = factor(Method, levels=a$Method),
-		Coincidence = factor(Coincidence, levels=b$Coincidence))
-		
-nodes <- data.frame(
-  name=c(
-  	as.character(links$Method),
-  	as.character(links$Coincidence)) %>% 
-    unique()
-  )
-
-links$IDMethod <- match(links$Method, nodes$name)-1 
-links$IDCoincidence <- match(links$Coincidence, nodes$name)-1
-
-sn<-sankeyNetwork(
-	Links = links,
-	Nodes = nodes,
-	Source = "IDMethod",
-	Target = "IDCoincidence",
-	Value = "n",
-	NodeID = "name", 
-	sinksRight=FALSE,
-	fontSize= 20,
-	nodeWidth = 20,
-	margin = list(left = 50),
-	colourScale = JS("d3.scaleOrdinal(d3.schemeCategory20b);"))
-
-onRender(
-  sn,
-  '
-  function(el, x) {
-    d3.selectAll(".node text").attr("text-anchor", "begin").attr("x", 20);
-  }
-  '
-)
-
